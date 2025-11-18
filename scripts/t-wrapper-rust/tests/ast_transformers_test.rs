a3400d09039418a62f1ab610c1640caba244c18c
manNomi <hanmw110@naver.com>
2025-11-18 12:46:50 +0900

    test: ast-transformers 테스트 코드 TypeScript와 1:1 매칭

--- scripts/t-wrapper-rust/tests/ast_transformers_test.rs
+++ scripts/t-wrapper-rust/tests/ast_transformers_test.rs
@@ -3,7 +3,7 @@
  * AST 변환 로직 테스트
  */
 
-use t_wrapper_rust::transform_function_body;
+use t_wrapper_rust::ast_transformers::{transform_function_body, TransformResult};
 use t_wrapper_rust::parser::{parse_file, ParseOptions};
 
 #[test]
@@ -14,8 +14,12 @@ fn transform_function_body_한국어_문자열_리터럴을_t_호출로_변환�
 }"#;
     
     // TODO: SWC AST traverse로 구현되면 실제 AST 노드로 테스트
+    let ast = parse_file(code, ParseOptions::default()).unwrap();
+    // TODO: AST traverse로 FunctionDeclaration 찾아서 transformFunctionBody 호출
+    // 현재는 소스코드 직접 검사로 테스트
     let result = transform_function_body((), code);
     assert!(result.was_modified);
+    let _ = ast;
 }
 
 #[test]
@@ -25,8 +29,12 @@ fn transform_function_body_한국어_템플릿_리터럴을_t_호출로_변환�
 }"#;
     
     // TODO: SWC AST traverse로 구현되면 실제 AST 노드로 테스트
+    let ast = parse_file(code, ParseOptions::default()).unwrap();
+    // TODO: AST traverse로 FunctionDeclaration 찾아서 transformFunctionBody 호출
+    // 현재는 소스코드 직접 검사로 테스트
     let result = transform_function_body((), code);
     assert!(result.was_modified);
+    let _ = ast;
 }
 
 #[test]
@@ -36,8 +44,12 @@ fn transform_function_body_한국어_JSXText를_t_호출로_변환해야_함() {
 }"#;
     
     // TODO: SWC AST traverse로 구현되면 실제 AST 노드로 테스트
+    let ast = parse_file(code, ParseOptions::default()).unwrap();
+    // TODO: AST traverse로 FunctionDeclaration 찾아서 transformFunctionBody 호출
+    // 현재는 소스코드 직접 검사로 테스트
     let result = transform_function_body((), code);
     assert!(result.was_modified);
+    let _ = ast;
 }
 
 #[test]
@@ -48,8 +60,10 @@ fn transform_function_body_이미_t로_래핑된_문자열은_변환하지_않�
     
     // TODO: SWC AST traverse로 구현되면 실제 AST 노드로 테스트
     // 현재는 t() 함수 감지 로직이 없으므로 false 반환
+    let ast = parse_file(code, ParseOptions::default()).unwrap();
     let result = transform_function_body((), code);
     assert!(!result.was_modified);
+    let _ = ast;
 }
 
 #[test]
@@ -61,7 +75,9 @@ fn transform_function_body_i18n_ignore_주석이_있으면_변환하지_않아�
     
     // TODO: SWC AST traverse로 구현되면 실제 AST 노드로 테스트
     // 현재는 주석 감지 로직이 없으므로 false 반환
+    let ast = parse_file(code, ParseOptions::default()).unwrap();
     let result = transform_function_body((), code);
     // TODO: 실제 구현 후 false로 변경
     // assert!(!result.was_modified);
+    let _ = ast;
 }
